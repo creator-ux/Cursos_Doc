d7396bd38bf3538f7ea46439e957f300a7066ec0
creator-ux <mauricionahuat400@gmail.com>
2025-05-22 22:26:28 -0600

    five commit

--- resources/views/components/admin.blade.php
+++ resources/views/components/admin.blade.php
@@ -1,5 +1,5 @@
 <nav class="sb-sidenav-menu-nested nav">
         <a class="nav-link" href="{{ route('record') }}">Registro</a> <!-- Aréa de registro a cursos -->
-        <a class="nav-link" href="{{ route('DownloadPdf') }}">Constancias Generador</a> <!-- Aréa de constancias -->
+        <!-- <a class="nav-link" href="{{ route('DownloadPdf') }}">Constancias Generador</a> Aréa de constancias PROXIMAMENTE -->
         <a class="nav-link" href="{{ route('periodos.index') }}">Periodos</a> <!-- Aréa de periodos -->
 </nav>
